2e7f4c81003a8576b7dd767f9f50a733765d50c6
zetaPRIME <qmanxt@gmail.com>
2017-05-12 05:42:41 -0400

    simple tap to toggle cell

--- source/Playfield.cpp
+++ source/Playfield.cpp
@@ -1,7 +1,10 @@
 #include "Playfield.h"
 
+#include <cmath>
+
 #include "starlight/ThemeManager.h"
 #include "starlight/GFXManager.h"
+#include "starlight/InputManager.h"
 
 using sl::Vector2;
 using sl::VRect;
@@ -9,20 +12,37 @@ using sl::Color;
 
 using sl::ThemeManager;
 using sl::GFXManager;
+using sl::InputManager;
 
 using namespace picrosspro;
 
 constexpr int width = 15;
 constexpr int height = 15;
+constexpr Vector2 cell = Vector2(12, 12);
+
+Playfield::Playfield(VRect rect) {
+    this->rect = rect;
+    field = std::vector<std::vector<int>>(width, std::vector<int>(height, 1));
+}
 
 void Playfield::Draw() {
     static auto tick = ThemeManager::GetAsset("app:/grid.tick");
-    Vector2 cell(12, 12);
     Vector2 off = rect.pos + GFXManager::GetOffset();
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             Vector2 pos = off + (Vector2(x,y) * cell);
-            tick->Draw(pos);
+            if (field[x][y] > 0) tick->Draw(pos);
         }
+    }
+}
+
+void Playfield::OnTouchOn() {
+    if (InputManager::Pressed(Keys::Touch)) {
+        //InputManager::GetDragHandle().Grab(this);
+        //MarkForRedraw();
+        Vector2 rel = InputManager::TouchPos() - ScreenRect().pos;
+        int tx = std::floor(rel.x / cell.x);
+        int ty = std::floor(rel.y / cell.y);
+        field[tx][ty] *= -1;
     }
 }
--- source/Playfield.h
+++ source/Playfield.h
@@ -1,18 +1,22 @@
 #pragma once
 
+#include <vector>
+
 #include "starlight/ui/UIElement.h"
 
 namespace picrosspro {
     class Playfield : public sl::ui::UIElement {
     public:
-        Playfield(sl::VRect rect) { this->rect = rect; }
-        Playfield(sl::Vector2 pos) { this->rect = sl::VRect(pos, sl::Vector2(15*12, 15*12)); }
+        std::vector<std::vector<int>> field;
+        
+        Playfield(sl::VRect rect);
+        Playfield(sl::Vector2 pos) : Playfield(sl::VRect(pos, sl::Vector2(15*12, 15*12))) { }
         ~Playfield() { }
         
         void Draw() override;
         
         // events
-        //void OnTouchOn() override;
+        void OnTouchOn() override;
         //void OnTouchOff() override;
         
         //void OnDragStart() override;
